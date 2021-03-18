a632ab3a5b2ec4b3f00853ecfe53945a591be8b6
GideonB-Ascent <gideon@ascentmanagement.ca>
2021-03-18 17:32:47 -0600

    created unauthenticated login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './Login.css';
 
-export default function Login() {
+async function loginUser(credentials) {
+  return fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+}
+
+export default function Login({setToken}) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    setToken(token)
+  }
   return (
     <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text"/>
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password"/>
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      <div className="form-wrapper">
+        <h1>Welcome</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-fields">
+            <label>
+              <p>Username</p>
+              <input type="text" onChange={e => setUserName(e.target.value)}/>
+            </label>
+            <label>
+              <p>Password</p>
+              <input type="password" onChange={e => setPassword(e.target.value)}/>
+            </label>
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
