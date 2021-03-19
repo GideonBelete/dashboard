b87988dbb351c46707a1f418126a1d9056176261
GideonB-Ascent <gideon@ascentmanagement.ca>
2021-03-19 16:55:54 -0600

    updated auth

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Main from './components/Main';
 import './App.css';
 
 function App() {
-  /*
+
     const [token, setToken] = useState();
     if(!token) {
       return <Login setToken={setToken} />
-    } */
+    }
     return (
       <div className="wrapper">
         <Router>
